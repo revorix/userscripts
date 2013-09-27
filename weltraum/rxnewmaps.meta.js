b7295d7b471265613ecb3470995f4958caaf21e0
42 <blue.ice@gmx.net>
2013-09-28 00:01:30 +0200

    weltraum/rxnewmaps: Initial script version
    The userscript as it is in use as of now.
    
    Signed-off-by: 42 <blue.ice@gmx.net>

---
+++ weltraum/rxnewmaps.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Revorix-Neue-Kartenansicht
+// @namespace   http://random.erdbeerkuchen.net/
+// @description Modifiziert die Kartenansicht für ein neues Fluggefühl im RX-Universum
+// @downloadURL http://random.erdbeerkuchen.net/rxcode/scripts/DarkIce/rxnewmaps.user.js
+// @updateURL   http://random.erdbeerkuchen.net/rxcode/scripts/DarkIce/rxnewmaps.meta.js
+// @version     0.9beta
+//
+// @include     http://87.106.151.92/*/map*.php*
+// @include     http://87.106.151.92/*/setup.php*
+// @include     http://87.106.151.92/*/sgfx_select.php*
+// @include     http://87.106.151.92/*/rx.php?set=4*
+// @include     http://87.106.151.92/*/rx.php?set=5*
+// @include     http://www.revorix.info/*/map*.php*
+// @include     http://www.revorix.info/*/setup.php*
+// @include     http://www.revorix.info/*/sgfx_select.php*
+// @include     http://www.revorix.info/*/rx.php?set=4*
+// @include     http://www.revorix.info/*/rx.php?set=5*
+// ==/UserScript==
+
+/* made by DarkIce */
