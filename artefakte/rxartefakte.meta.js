ec448e6e73d89000afcdc905a8534e03c616c4fb
[42] <blue.ice@gmx.net>
2017-06-02 00:49:38 +0200

    artefakte/rxartefakte: update URI and author metadata
    Signed-off-by: [42] <blue.ice@gmx.net>

--- artefakte/rxartefakte.meta.js
+++ artefakte/rxartefakte.meta.js
@@ -1,16 +1,13 @@
 // ==UserScript==
 // @name        Revorix-Artefakte
 // @namespace   http://random.erdbeerkuchen.net/
+// @author      DarkIce (The42)
 // @description Addon zur Generierung einer Artefakt-Übersicht
-// @downloadURL http://random.erdbeerkuchen.net/rxcode/scripts/DarkIce/rxartefakte.user.js
-// @updateURL   http://random.erdbeerkuchen.net/rxcode/scripts/DarkIce/rxartefakte.meta.js
+// @downloadURL https://raw.githubusercontent.com/tpummer/gm-revorix/master/artefakte/rxartefakte.user.js
+// @updateURL   https://raw.githubusercontent.com/tpummer/gm-revorix/master/artefakte/rxartefakte.meta.js
 // @grant       none
 // @version     0.42.9
 //
 // @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/artefakte.*/
 // @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/setup\.php/
 // ==/UserScript==
-
-/* made by DarkIce */
-
-/* Meta data for automatic updates */
