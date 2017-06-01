fd023c51f38163484fe354d07e15ae894155bb86
42 <blue.ice@gmx.net>
2017-06-02 01:15:55 +0200

    weltraum/rxnewmaps: Update URIs
    Signed-off-by: 42 <blue.ice@gmx.net>

--- weltraum/rxnewmaps.meta.js
+++ weltraum/rxnewmaps.meta.js
@@ -5,18 +5,12 @@
 // @downloadURL http://random.erdbeerkuchen.net/rxcode/scripts/DarkIce/rxnewmaps.user.js
 // @updateURL   http://random.erdbeerkuchen.net/rxcode/scripts/DarkIce/rxnewmaps.meta.js
 // @priority	-1
-// @version     0.9beta
+// @version     0.10beta
 //
-// @include     http://87.106.151.92/*/map*.php*
-// @include     http://87.106.151.92/*/setup.php*
-// @include     http://87.106.151.92/*/sgfx_select.php*
-// @include     http://87.106.151.92/*/rx.php?set=4*
-// @include     http://87.106.151.92/*/rx.php?set=5*
-// @include     http://www.revorix.info/*/map*.php*
-// @include     http://www.revorix.info/*/setup.php*
-// @include     http://www.revorix.info/*/sgfx_select.php*
-// @include     http://www.revorix.info/*/rx.php?set=4*
-// @include     http://www.revorix.info/*/rx.php?set=5*
+// @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/map.*\.php/
+// @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/setup\.php/
+// @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/sgfx\_select\.php/
+// @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/rx\.php\?set=(4|5)/
 // ==/UserScript==
 
 /* made by DarkIce */
