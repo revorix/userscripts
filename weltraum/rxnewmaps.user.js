98add8adb2cf4941e84c8a4b413be13a5c3b13e2
42 <blue.ice@gmx.net>
2017-06-02 01:17:46 +0200

    weltraum/rxnewmaps: Metadata fixes
    Signed-off-by: 42 <blue.ice@gmx.net>

--- weltraum/rxnewmaps.user.js
+++ weltraum/rxnewmaps.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name        Revorix-Neue-Kartenansicht
 // @namespace   http://random.erdbeerkuchen.net/
+// @author      DarkIce (The42)
 // @description Modifiziert die Kartenansicht für ein neues Fluggefühl im RX-Universum
 // @downloadURL http://random.erdbeerkuchen.net/rxcode/scripts/DarkIce/rxnewmaps.user.js
 // @updateURL   http://random.erdbeerkuchen.net/rxcode/scripts/DarkIce/rxnewmaps.meta.js
-// @priority	-1
+// @priority    -1
+// @grant       None
 // @version     0.10beta
 //
 // @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/map.*\.php/
@@ -12,8 +14,6 @@
 // @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/sgfx\_select\.php/
 // @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/rx\.php\?set=(4|5)/
 // ==/UserScript==
-
-/* made by DarkIce */
 
 /* globals console */
 
