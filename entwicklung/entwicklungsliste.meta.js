c36cc93ab82a142d421d7e82e097dfbd85369366
[42] <blue.ice@gmx.net>
2017-06-01 23:41:30 +0200

    entwicklung: add *.meta.js for entwicklungsliste
    GM will look for a <script>.meta.js when checking for updates. We can
    use that to make renaming possible without breaking auto-updates.
    
    Signed-off-by: [42] <blue.ice@gmx.net>

---
+++ entwicklung/entwicklungsliste.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name Entwicklungsliste
+// @namespace coolcow.dyndns.org
+// @description Script zum Uebertrag von Schiffsplaenen aus dem Sternenbund nach Revorix
+// @author coolius
+// @contributor Lord-FaKe
+// @contributor   Wintermoon
+// @downloadURL https://raw.githubusercontent.com/tpummer/gm-revorix/master/entwicklung/Revorix_Sternenbund_Importer.user.js
+// @updateURL https://raw.githubusercontent.com/tpummer/gm-revorix/master/entwicklung/Revorix_Sternenbund_Importer.user.js
+// @include http*revorix.info/php/entwicklung_neu.php*
+// @version 3.2017040102
+// @run-at document-idle
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant unsafeWindow
+// ==/UserScript==
