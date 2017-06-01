8e7b47ead8e5da8a219165d71a21c1deaf53f678
[42] <blue.ice@gmx.net>
2017-06-02 00:51:22 +0200

    artefakte/rxartefakte 1.20170602
    Signed-off-by: [42] <blue.ice@gmx.net>

--- artefakte/rxartefakte.user.js
+++ artefakte/rxartefakte.user.js
@@ -6,7 +6,7 @@
 // @downloadURL https://raw.githubusercontent.com/tpummer/gm-revorix/master/artefakte/rxartefakte.user.js
 // @updateURL   https://raw.githubusercontent.com/tpummer/gm-revorix/master/artefakte/rxartefakte.meta.js
 // @grant       none
-// @version     0.42.9
+// @version     1.20170602
 //
 // @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/artefakte.*/
 // @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/setup\.php/
