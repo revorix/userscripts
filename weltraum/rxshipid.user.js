7411ba2843192c4c25bc4baab3de1cd322c12b1d
[42] <blue.ice@gmx.net>
2017-06-02 14:02:39 +0200

    weltraum/rxshipid: metadata update
    Signed-off-by: [42] <blue.ice@gmx.net>

--- weltraum/rxshipid.user.js
+++ weltraum/rxshipid.user.js
@@ -1,16 +1,15 @@
 ﻿// ==UserScript==
 // @name        Revorix-Schiffsalter-Berechnung
 // @namespace   http://random.erdbeerkuchen.net/
+// @author      DarkIce (The42)
 // @description Zeigt das geschätzte Alter eines Schiffes im Flottenscan an
-// @downloadURL http://random.erdbeerkuchen.net/rxcode/scripts/DarkIce/rxshipid.user.js
-// @updateURL   http://random.erdbeerkuchen.net/rxcode/scripts/DarkIce/rxshipid.meta.js
+// @downloadURL https://raw.githubusercontent.com/tpummer/gm-revorix/master/weltraum/rxshipid.user.js
+// @updateURL   https://raw.githubusercontent.com/tpummer/gm-revorix/master/weltraum/rxshipid.meta.js
 // @version     1.0.20120813
+// @grant       None
 //
-// @include     http://87.106.151.92/*/map_fflotte.php*
-// @include     http://www.revorix.info/*/map_fflotte.php*
+// @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/map_fflotte\.php/
 // ==/UserScript==
-
-/* made by DarkIce */
 
 // Referenzen - ShipID + Bautag aus dem Logbuch (vorsicht bei Baubeschleunigern!) - Monate sind 0-basiert!
 var refs = new Array();
