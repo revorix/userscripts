90672ed48908d1555ced1f6c9c68457409a7b47f
[42] <blue.ice@gmx.net>
2017-06-10 16:57:30 +0200

    entwicklung/rxbauplanmod: add values for level 30
    Add a third column containing characteristics for when the ship has been
    built and leveled up to 30. This allows better comparison with existing
    ships, which likely will already have reached that level.
    
    Signed-off-by: [42] <blue.ice@gmx.net>

--- entwicklung/rxbauplanmod.user.js
+++ entwicklung/rxbauplanmod.user.js
@@ -6,7 +6,7 @@
 // @downloadURL https://raw.githubusercontent.com/tpummer/gm-revorix/master/entwicklung/rxbauplanmod.user.js
 // @updateURL   https://raw.githubusercontent.com/tpummer/gm-revorix/master/entwicklung/rxbauplanmod.meta.js
 // @grant       None
-// @version     1.20170610
+// @version     1.20170610.1
 //
 // @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/entwicklung_i\.php/
 // ==/UserScript==
@@ -68,31 +68,48 @@
 
 		var baukp = Math.ceil(0.2 * ship_kp[1]) + 5;
 
+		var level30 = {};
+
+		var minkp = ship_type[1].trim().startsWith('Korvette') ? 10 : 15;
+		level30.kp = Math.max(Math.round(0.7 * ship_kp[1]), minkp);
+		level30.aw = Math.round(1.3 * ship_aw[1]);
+		level30.sh = Math.round(1.3 * ship_sh[1]);
+		level30.pz = Math.round(1.3 * ship_pz[1]);
+		level30.str = Math.round(1.3 * ship_str[1]);
+		level30.wnd = Math.round(1.3 * ship_wnd[1]);
+		level30.sens = Math.round(1.3 * ship_sens[1]);
+		level30.trn = Math.round(1.3 * ship_trn[1]);
+		level30.minp = Math.round(0.93 * ship_minp[1]);
+		level30.rea = Math.round(1.3 * ship_rea[1]);
+		level30.lr = Math.round(1.3 * ship_lr[1]);
+
 		planvals.innerHTML = (
-			"<tr><td class=\"nfo\" align=middle colspan=2><b>Technische Daten des Bauplans</b></td></tr>" +
-			"<tr><td width=200>" + ship_name[0] + "</td><td>" + ship_name[1] + "</td></tr>" +
-			"<tr><td>" + ship_type[0] + "</td><td>" + ship_type[1] + "</td></tr>" +
-			"<tr><td>" + ship_opt[0] + "</td><td>" + ship_opt[1] + "</td></tr>" +
-			"<tr><td>" + ship_build[0] + "</td><td>" + ship_build[1] + "</td></tr>" +
-			"<tr><td>" + ship_tl[0] + "</td><td>" + ship_tl[1] + "</td></tr>" +
-			"<tr><td>" + ship_mod[0] + "</td><td>" + ship_mod[1] + "</td></tr>" +
-			"<tr><td>" + ship_kp[0] + "</td><td>" + ship_kp[1] + "&nbsp;<span style='color:#00FFFF'>(" + baukp + " im Bau)</span>" + "</td></tr>" +
-			"<tr><td>" + ship_ss[0] + "</td><td>" + ship_ss[1] + "</td></tr>" +
-			"<tr><td>" + ship_sq[0] + "</td><td>" + ship_sq[1] + "</td></tr>" +
-			"<tr><td>" + ship_sv[0] + "</td><td>" + ship_sv[1] + "</td></tr>" +
-			"<tr><td>" + ship_sz[0] + "</td><td>" + ship_sz[1] + "</td></tr>" +
-			"<tr><td>" + ship_az[0] + "</td><td>" + ship_az[1] + "</td></tr>" +
-			"<tr><td>" + ship_ae[0] + "</td><td>" + ship_ae[1] + "</td></tr>" +
-			"<tr><td>" + ship_aw[0] + "</td><td>" + ship_aw[1] + "</td></tr>" +
-			"<tr><td>" + ship_sh[0] + "</td><td>" + ship_sh[1] + "</td></tr>" +
-			"<tr><td>" + ship_pz[0] + "</td><td>" + ship_pz[1] + "</td></tr>" +
-			"<tr><td>" + ship_str[0] + "</td><td>" + ship_str[1] + "</td></tr>" +
-			"<tr><td>" + ship_wnd[0] + "</td><td>" + ship_wnd[1] + "</td></tr>" +
-			"<tr><td>" + ship_sens[0] + "</td><td>" + ship_sens[1] + "</td></tr>" +
-			"<tr><td>" + ship_trn[0] + "</td><td>" + ship_trn[1] + "</td></tr>" +
-			"<tr><td>" + "Besatzung" + "</td><td>" + ship_minp[1] + "/" + ship_maxp[1] + "</td></tr>" +
-			"<tr><td>" + ship_rea[0] + "</td><td>" + ship_rea[1] + "</td></tr>" +
-			"<tr><td>" + ship_lr[0] + "</td><td>" + ship_lr[1] + "</td></tr>"
+			"<tr><td class=\"nfo\" align=middle colspan=3><b>Technische Daten des Bauplans</b></td></tr>" +
+			"<tr><td width=200>" + ship_name[0] + "</td><td>" + ship_name[1] + "</td><td><i>Level 30</i></td></tr>" +
+			"<tr><td>" + ship_type[0] + "</td><td colspan=2>" + ship_type[1] + "</td></tr>" +
+			"<tr><td>" + ship_opt[0] + "</td><td colspan=2>" + ship_opt[1] + "</td></tr>" +
+			"<tr><td>" + ship_build[0] + "</td><td colspan=2>" + ship_build[1] + "</td></tr>" +
+			"<tr><td>" + ship_tl[0] + "</td><td colspan=2>" + ship_tl[1] + "</td></tr>" +
+			"<tr><td>" + ship_mod[0] + "</td><td colspan=2>" + ship_mod[1] + "</td></tr>" +
+			"<tr><td>" + ship_kp[0] + "</td><td>" + ship_kp[1] + "&nbsp;<span style='color:#00FFFF'>(" + baukp + " im Bau)</span>" +
+				"</td><td><i>" + level30.kp + "</i></td></tr>" +
+			"<tr><td>" + ship_ss[0] + "</td><td colspan=2>" + ship_ss[1] + "</td></tr>" +
+			"<tr><td>" + ship_sq[0] + "</td><td colspan=2>" + ship_sq[1] + "</td></tr>" +
+			"<tr><td>" + ship_sv[0] + "</td><td colspan=2>" + ship_sv[1] + "</td></tr>" +
+			"<tr><td>" + ship_sz[0] + "</td><td colspan=2>" + ship_sz[1] + "</td></tr>" +
+			"<tr><td>" + ship_az[0] + "</td><td colspan=2>" + ship_az[1] + "</td></tr>" +
+			"<tr><td>" + ship_ae[0] + "</td><td colspan=2>" + ship_ae[1] + "</td></tr>" +
+			"<tr><td>" + ship_aw[0] + "</td><td>" + ship_aw[1] + "</td><td><i>" + level30.aw + "</i></td></tr>" +
+			"<tr><td>" + ship_sh[0] + "</td><td>" + ship_sh[1] + "</td><td><i>" + level30.sh + "</i></td></tr>" +
+			"<tr><td>" + ship_pz[0] + "</td><td>" + ship_pz[1] + "</td><td><i>" + level30.pz + "</i></td></tr>" +
+			"<tr><td>" + ship_str[0] + "</td><td>" + ship_str[1] + "</td><td><i>" + level30.str + "</i></td></tr>" +
+			"<tr><td>" + ship_wnd[0] + "</td><td>" + ship_wnd[1] + "</td><td><i>" + level30.wnd + "</i></td></tr>" +
+			"<tr><td>" + ship_sens[0] + "</td><td>" + ship_sens[1] + "</td><td><i>" + level30.sens + "</i></td></tr>" +
+			"<tr><td>" + ship_trn[0] + "</td><td>" + ship_trn[1] + "</td><td><i>" + level30.trn + "</i></td></tr>" +
+			"<tr><td>" + "Besatzung" + "</td><td>" + ship_minp[1] + "/" + ship_maxp[1] + "</td><td><i>" +
+				level30.minp + "/" + ship_maxp[1] + "</i></td></tr>" +
+			"<tr><td>" + ship_rea[0] + "</td><td>" + ship_rea[1] + "</td><td><i>" + level30.rea + "</i></td></tr>" +
+			"<tr><td>" + ship_lr[0] + "</td><td>" + ship_lr[1] + "</td><td><i>" + level30.lr + "</i></td></tr>"
 		);
 
 		window.resizeBy(-150, 250);
