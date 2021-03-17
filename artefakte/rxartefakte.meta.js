cb2fb88d467c3ad88b124c257659321ec0baed8f
Thomas Pummer <tpummer@users.noreply.github.com>
2021-03-17 13:59:15 +0100

    changed to new domain

--- artefakte/rxartefakte.meta.js
+++ artefakte/rxartefakte.meta.js
@@ -6,7 +6,7 @@
 // @downloadURL https://raw.githubusercontent.com/tpummer/gm-revorix/master/artefakte/rxartefakte.user.js
 // @updateURL   https://raw.githubusercontent.com/tpummer/gm-revorix/master/artefakte/rxartefakte.meta.js
 // @grant       none
-// @version     1.20170602.1
+// @version     1.20210317.1
 //
 // @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/artefakte.*/
 // @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/setup\.php/
