404afcc6307a3b0abdf2398b6f65ddbddf11021d
Thomas Pummer <tpummer@users.noreply.github.com>
2021-09-19 00:53:46 +0200

    update game domain

--- artefakte/rxartefakte.meta.js
+++ artefakte/rxartefakte.meta.js
@@ -6,8 +6,8 @@
 // @downloadURL https://raw.githubusercontent.com/tpummer/gm-revorix/master/artefakte/rxartefakte.user.js
 // @updateURL   https://raw.githubusercontent.com/tpummer/gm-revorix/master/artefakte/rxartefakte.meta.js
 // @grant       none
-// @version     1.20210317.1
+// @version     1.20210919.1
 //
-// @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/artefakte.*/
-// @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/setup\.php/
+// @include     /(87\.106\.151\.92|(game\.|www\.)?revorix\.(de|com|info))\S*\/artefakte.*/
+// @include     /(87\.106\.151\.92|(game\.|www\.)?revorix\.(de|com|info))\S*\/setup\.php/
 // ==/UserScript==
