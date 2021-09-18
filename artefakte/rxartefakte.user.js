4f6ac542b3dc25d0da72d56a5aacb6a96c491520
Thomas Pummer <tpummer@users.noreply.github.com>
2021-09-19 00:53:08 +0200

    update game domain

--- artefakte/rxartefakte.user.js
+++ artefakte/rxartefakte.user.js
@@ -6,10 +6,10 @@
 // @downloadURL https://raw.githubusercontent.com/tpummer/gm-revorix/master/artefakte/rxartefakte.user.js
 // @updateURL   https://raw.githubusercontent.com/tpummer/gm-revorix/master/artefakte/rxartefakte.meta.js
 // @grant       none
-// @version     1.20210317.1
+// @version     1.20210919.1
 //
-// @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/artefakte.*/
-// @include     /(87\.106\.151\.92|(www\.)?revorix\.(de|com|info))\S*\/setup\.php/
+// @include     /(87\.106\.151\.92|(game\.|www\.)?revorix\.(de|com|info))\S*\/artefakte.*/
+// @include     /(87\.106\.151\.92|(game\.|www\.)?revorix\.(de|com|info))\S*\/setup\.php/
 // ==/UserScript==
 
 /* jshint devel: true */
@@ -881,8 +881,8 @@
 	{
 		if ((/\/\/\d+\.\d+\.\d+\.\d+\//).test(document.URL)) {	/* IP */
 			return (document.URL.match(/[\w:\.\/]+\/rx\//)[0]);
-		} else {	/* revorix.info */
-			return (document.URL.match(/[\w:\.\/]+revorix\.com\//)[0]);
+		} else {	/* game.revorix.de */
+			return (document.URL.match(/[\w:\.\/]+revorix\.de\//)[0]);
 		}
 	}
 
