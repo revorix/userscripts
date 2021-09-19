6bf5a0ad8597b87967e1bcc45fd2efe72c223a43
Tsueah <cy4n1d3@gmail.com>
2021-09-19 22:38:15 +0200

    Update Bauzeit-Script

---
+++ bauzeiten/revorix.bauzeit.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            Revorix Bauzeit-Script
+// @namespace       http://toasten.de/
+// @description     Dieses Script erweitert die Bau-Anzeige. Es zeigt an, wann ein Bau abgeschlossen wird.
+// @version         3.2.210919
+//
+// @updateURL       https://github.com/tpummer/gm-revorix/raw/master/bauzeiten/revorix.bauzeit.meta.js
+// @downloadURL     https://github.com/tpummer/gm-revorix/raw/master/bauzeiten/revorix.bauzeit.user.js
+//
+// @grant           none
+// @include         http*revorix.de/*/bau.php*
+// ==/UserScript==
